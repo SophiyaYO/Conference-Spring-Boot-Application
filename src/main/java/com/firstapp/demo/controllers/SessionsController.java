42f77356f63cb5f4bf67a8cd7c74d11e3d7782e7
Sofia Yo <dimitrova.sophiya@gmail.com>
2020-08-20 14:22:56 +0300

    added CREATE end point (Post Mapping)

--- src/main/java/com/firstapp/demo/controllers/SessionsController.java
+++ src/main/java/com/firstapp/demo/controllers/SessionsController.java
@@ -3,10 +3,8 @@ package com.firstapp.demo.controllers;
 import com.firstapp.demo.models.Session;
 import com.firstapp.demo.repositories.SessionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.Lob;
 import java.util.List;
 
 @RestController//this will respond to payloads incoming and outgoing as json rest points
@@ -25,9 +23,14 @@ public class SessionsController {
     @GetMapping//tells exactly which http verb to use
     // which will be a get verb to call this end point
     @RequestMapping("{id}")
-    public Session get(@PathVariable Long id){
+    public Session get(@PathVariable Long id) {
         return sessionRepository.getOne(id);
     }//adding an additional id to the url for the GET method
+
+    @PostMapping
+    public Session create(@RequestBody final Session session) {
+        return sessionRepository.saveAndFlush(session);
+    }
 
 //    @PostMapping
 //    @ResponseStatus(HttpStatus.CREATED)//what I want to occur when the method executes and finishes
